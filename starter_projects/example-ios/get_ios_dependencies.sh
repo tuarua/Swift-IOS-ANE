752781cdbe5047901bd6c56ebf4b8df71e19a19c
Eoin Landy <el111@yahoo.com>
2018-03-30 11:10:14 +0100

    2.4.0

--- starter_projects/example-ios/get_ios_dependencies.sh
+++ starter_projects/example-ios/get_ios_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.3.0/ios_dependencies.zip
+rm -r ios_dependencies/device
+rm -r ios_dependencies/simulator
+
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0-beta/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
